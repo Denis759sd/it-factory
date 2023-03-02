c053d4fac5fb77652471f96f5be38b60b09ace0a
Dzianis Andrushchanka <dzianis.andrushchanka@cogniteq.com>
2023-03-03 00:12:09 +0300

    update address

--- src/config/default.js
+++ src/config/default.js
@@ -2,7 +2,7 @@ module.exports = {
     apiVersion: 1,
     apiUri: '/api',
     port: process.env.PORT || 8000,
-    address: process.env.ADDRESS,
+    address: process.env.ADDRESS || '3.216.88.24',
     db: {
         username: '',
         password: '',
