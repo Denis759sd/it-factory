9df5ca877cd475f43d4d071b6250693f4f07c324
Dzianis Andrushchanka <dzianis.andrushchanka@cogniteq.com>
2023-03-02 23:52:24 +0300

    add address

--- src/config/default.js
+++ src/config/default.js
@@ -2,7 +2,7 @@ module.exports = {
     apiVersion: 1,
     apiUri: '/api',
     port: process.env.PORT || 8000,
-    address: process.env.ADDRESS,
+    address: process.env.ADDRESS || 'it-factory.herokuapp.com',
     db: {
         username: '',
         password: '',
